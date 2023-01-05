6826c9ee7fb6096a2a5c9271cfd984d0915d7053
adno88 <adno88@o2.pl>
2023-01-05 19:05:22 +0100

    Added const's in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const currentNumber = document.querySelector('.currentNumber');
+const previousNumber = document.querySelector('.previousNumber p');
+const sign = document.querySelector('.screenSign');
+const buttonsNumbers = document.querySelectorAll('.number');
+const buttonsOperator = document.querySelectorAll('operator');
+const equals = document.querySelector('.equals');
+const buttonClear = document.querySelector('.clear');
+
+let result = '';
