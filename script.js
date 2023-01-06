333adae83b1cab31c1531f76446c9de46339b5c5
adno88 <adno88@o2.pl>
2023-01-06 15:08:54 +0100

    In script.js added addEventListener to every const's

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const currentNumber = document.querySelector('.currentNumber');
 const previousNumber = document.querySelector('.previousNumber p');
 const sign = document.querySelector('.screenSign');
 const buttonsNumbers = document.querySelectorAll('.number');
-const buttonsOperator = document.querySelectorAll('operator');
+const buttonsOperators = document.querySelectorAll('operator');
 const equals = document.querySelector('.equals');
-const buttonClear = document.querySelector('.clear');
+const clear = document.querySelector('.clear');
 
 let result = '';
+
+//nasłuchiwacze
+
+buttonsOperators.forEach((button) => button.addEventListener('click', calculate))
+
+equals.addEventListener('click', result);
+
+clear.addEventListener('click', clearScreen);
+
+buttonsNumbers.forEach((button) => {
+    button.addEventListener('click', showNumbers)
+})
